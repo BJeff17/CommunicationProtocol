46e41ebae5555f325b5f1be66d35836651ed42c6
Jeff Vieyra <166553095+BJeff17@users.noreply.github.com>
2025-12-29 11:25:57 +0100

    Refactor client connection to use ADDRESSE variable

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 
 import socket
 
+ADDRESSE = ("localhost",8000)
+
+
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost",8000))
+client.connect(ADDRESSE)
 print(client.recv(512).decode())
 
 
@@ -11,11 +14,13 @@ while True :
     out = client.recv(512).decode()
     print(out)
     if out=="quit":
+        print("Vous vous déconnecté!")
         break
     message = input("Entrez votre message: ")
    
     client.sendall(message.encode())
     if message == "quit" :
+        print("L'autre s'est déconnecté!")
         break
 
 
